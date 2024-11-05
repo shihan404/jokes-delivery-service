28ec607705b2131e050383a09bda6a38a99fe1ba
Shihan98 <shihanthathsara98@gmail.com>
2024-11-05 14:57:59 +0530

    fix:origin issue

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,9 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:3000', // Replace with your client's URL or use '*' to allow all
+    origin: '*',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: false, // Allow cookies if needed
+    credentials: false,
   });
   await app.listen(process.env.PORT ?? 3000);
 }
